053debb2f8703a6fc300ecc5dd23ea6dd33baf51
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-09-26 20:31:20 -0500

    core: create skeleton for routes

--- server.js
+++ server.js
@@ -31,3 +31,40 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI, function(err, database) {
     console.log("App now running on port", port);
   });
 });
+
+// CONTACTS API ROUTES BELOW
+
+// Generic error handler used by all endpoints.
+function handleError(res, reason, message, code) {
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({"error": message});
+}
+
+/*  "/api/contacts"
+    GET: finds all contacts
+    POST: creates a new contact
+*/
+app.get("/api/contacts", function(req, res) {
+
+});
+
+app.post("/api/contacts", function(req, res){
+
+});
+
+/*  "/api/contacts/:id"
+    GET: find contact by id
+    PUT: update contact by id
+    DELETE: deletes contact by id
+*/
+app.get("/api/contacts/:id", function(req, res){
+
+});
+
+app.put("/api/contacts/:id", function(req, res){
+
+});
+
+app.delete("/api/contacts/:id", function(req, res){
+
+});
